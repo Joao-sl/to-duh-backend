b83631dec841aa7c79986b767c694da3680e6f63
João <joaosl.dev@gmail.com>
2026-02-06 15:36:05 -0300

    feat: Configure TypeORM with database settings and environment variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import databaseConfig from './config/database.config';
+import { ConfigModule, ConfigType } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({}),
+    ConfigModule.forFeature(databaseConfig),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(databaseConfig)],
+      inject: [databaseConfig.KEY],
+      useFactory: (databaseConfigs: ConfigType<typeof databaseConfig>) => ({
+        type: databaseConfigs.type,
+        host: databaseConfigs.host,
+        port: databaseConfigs.port,
+        database: databaseConfigs.database,
+        username: databaseConfigs.username,
+        password: databaseConfigs.password,
+        synchronize: databaseConfigs.synchronize,
+        autoLoadEntities: databaseConfigs.autoLoadEntities,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
