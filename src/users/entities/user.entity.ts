ccd312e2683b2a9cac3dcac158052e13dbd5e6e1
João <joaosl.dev@gmail.com>
2026-02-06 15:51:37 -0300

    feat: Add scaffold users resource
    - Create `users.module.ts` with basic structure
    - Create `users.controller.ts` with basic structure
    - Create `users.service.ts` with basic structure
    - Create `user.entity.ts` with user columns

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ length: 128 })
+  password: string;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column({ default: true })
+  is_active: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
