4e27bf8529871cd5e47da8fe9b1e5ab62f364fd1
João <joaosl.dev@gmail.com>
2026-02-09 21:03:21 -0300

    feat: Add global ValidationPipe and serialization interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
