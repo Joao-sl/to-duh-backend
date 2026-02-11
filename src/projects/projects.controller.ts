2e71f7848aa22250b824dc682399535f2e0572e2
João <joaosl.dev@gmail.com>
2026-02-11 17:59:49 -0300

    feat: Implement create project functionality to `AuthController` and `AuthService`
    - Add to route to create Projects
    - Add `create` to persist new entity and return it
    - Add Serialization options to return `ResponseProjectDto`

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { ProjectsService } from './projects.service';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ResponseProjectDto } from './dto/response-project.dto';
+import { CurrentAuthToken } from 'src/auth/params/current-auth-token.param';
+import { JwtAccessTokenPayloadDto } from 'src/auth/dto/jwt-token-payload.dto';
+import {
+  Body,
+  Controller,
+  Post,
+  SerializeOptions,
+  UseGuards,
+} from '@nestjs/common';
 
-@Controller()
-export class ProjectsController {}
+@SerializeOptions({ type: ResponseProjectDto })
+@Controller('projects')
+@UseGuards(JwtAuthGuard)
+export class ProjectsController {
+  constructor(private readonly projectsService: ProjectsService) {}
+
+  @Post()
+  createProject(
+    @CurrentAuthToken() currentToken: JwtAccessTokenPayloadDto,
+    @Body() data: CreateProjectDto,
+  ) {
+    return this.projectsService.create(currentToken, data);
+  }
+}
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,29 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './entities/project.entity';
+import { UsersService } from 'src/users/users.service';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { JwtAccessTokenPayloadDto } from 'src/auth/dto/jwt-token-payload.dto';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectsRepo: Repository<Project>,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async create(jwtPayload: JwtAccessTokenPayloadDto, data: CreateProjectDto) {
+    const user = await this.usersService.findOneById(jwtPayload.sub);
+
+    const projectPayload = {
+      user_id: user.id,
+      ...data,
+    };
+
+    const project = await this.projectsRepo.save(projectPayload);
+
+    return project;
+  }
+}
