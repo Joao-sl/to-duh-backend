d493813211c831bd6645586634df0b00e15dea6b
João <joaosl.dev@gmail.com>
2026-02-11 16:41:41 -0300

    feat: Implement Trim decorator for string trimming in transformation

---
+++ src/common/decorators/trim.decorator.ts
@@ -0,0 +1,9 @@
+import { Transform, TransformFnParams } from 'class-transformer';
+
+export const Trim = () =>
+  Transform(({ value }: TransformFnParams) => {
+    if (typeof value === 'string') {
+      return value.trim();
+    }
+    return value as unknown;
+  });
