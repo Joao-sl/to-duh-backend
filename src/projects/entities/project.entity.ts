622bb215a7062ca834a92310002756201c51a0b1
João <joaosl.dev@gmail.com>
2026-02-11 12:00:04 -0300

    feat: Add Project controller, service, module and entity

---
+++ src/projects/entities/project.entity.ts
@@ -0,0 +1,36 @@
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Project {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user_id: User;
+
+  @Column({ length: 80 })
+  name: string;
+
+  @Column({ length: 255, nullable: true })
+  description: string;
+
+  @Column({ default: false })
+  is_favorite: boolean;
+
+  @Column({ default: false })
+  is_archived: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
