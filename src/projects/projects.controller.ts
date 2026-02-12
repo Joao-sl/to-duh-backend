0b2478d742d7e7afac05868132540a22d25c2c19
João <joaosl.dev@gmail.com>
2026-02-12 11:38:03 -0300

    feat: Implement project update and delete endpoints
    - Implement methods in AuthController and AuthService
    - Add PATCH endpoint for partial project updates
    - Add DELETE endpoint to remove project by ID

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,5 +1,6 @@
 import { ProjectsService } from './projects.service';
 import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { ResponseProjectDto } from './dto/response-project.dto';
 import { CurrentAuthToken } from 'src/auth/params/current-auth-token.param';
@@ -7,9 +8,13 @@ import { JwtAccessTokenPayloadDto } from 'src/auth/dto/jwt-token-payload.dto';
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
   SerializeOptions,
   UseGuards,
@@ -40,5 +45,23 @@ export class ProjectsController {
     @Body() data: CreateProjectDto,
   ) {
     return this.projectsService.create(currentToken, data);
+  }
+
+  @Patch(':id')
+  updateProject(
+    @CurrentAuthToken() currentToken: JwtAccessTokenPayloadDto,
+    @Body() data: UpdateProjectDto,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.projectsService.update(currentToken, id, data);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteProject(
+    @CurrentAuthToken() currentToken: JwtAccessTokenPayloadDto,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.projectsService.deleteById(currentToken, id);
   }
 }
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -3,8 +3,13 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
 import { UsersService } from 'src/users/users.service';
 import { CreateProjectDto } from './dto/create-project.dto';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { UpdateProjectDto } from './dto/update-project.dto';
 import { JwtAccessTokenPayloadDto } from 'src/auth/dto/jwt-token-payload.dto';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 
 @Injectable()
 export class ProjectsService {
@@ -51,5 +56,42 @@ export class ProjectsService {
     const project = await this.projectsRepo.save(projectPayload);
 
     return project;
+  }
+
+  async update(
+    jwtPayload: JwtAccessTokenPayloadDto,
+    id: number,
+    data: UpdateProjectDto,
+  ) {
+    if (Object.values(data).length === 0) {
+      throw new BadRequestException('Request body is empty');
+    }
+
+    const user = await this.usersService.findOneById(jwtPayload.sub);
+    const project = await this.projectsRepo.findOne({
+      where: {
+        id: id,
+        user: { id: user.id },
+      },
+    });
+
+    if (!project) throw new NotFoundException('Project not found');
+
+    const merged = this.projectsRepo.merge(project, data);
+    const updatedProject = await this.projectsRepo.save(merged);
+
+    return updatedProject;
+  }
+
+  async deleteById(jwtPayload: JwtAccessTokenPayloadDto, id: number) {
+    const user = await this.usersService.findOneById(jwtPayload.sub);
+    const project = await this.projectsRepo.delete({
+      id: id,
+      user: { id: user.id },
+    });
+
+    if (project.affected === 0) {
+      throw new NotFoundException('Project not found');
+    }
   }
 }
