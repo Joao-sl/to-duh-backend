8497ba695c145b1afee73270ce45739bbe9b514f
João <joaosl.dev@gmail.com>
2026-02-12 16:12:50 -0300

    feat: Implement endpoint to create a section in AuthController and AuthService
    - Add `ResponseProjectDto` to serializer options to standardize responses

--- src/sections/sections.controller.ts
+++ src/sections/sections.controller.ts
@@ -1,6 +1,28 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { SectionsService } from './sections.service';
+import { CreateSectionDto } from './dto/create-section.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { JwtAccessTokenPayloadDto } from 'src/auth/dto/jwt-token-payload.dto';
+import { CurrentAuthToken } from 'src/auth/params/current-auth-token.param';
+import { ResponseProjectDto } from 'src/projects/dto/response-project.dto';
+import {
+  Body,
+  Controller,
+  Post,
+  SerializeOptions,
+  UseGuards,
+} from '@nestjs/common';
 
 @Controller('sections')
 @UseGuards(JwtAuthGuard)
-export class SectionsController {}
+@SerializeOptions({ type: ResponseProjectDto })
+export class SectionsController {
+  constructor(private readonly sectionsService: SectionsService) {}
+
+  @Post()
+  createSection(
+    @CurrentAuthToken() currentToken: JwtAccessTokenPayloadDto,
+    @Body() data: CreateSectionDto,
+  ) {
+    return this.sectionsService.create(currentToken, data);
+  }
+}
--- src/sections/sections.service.ts
+++ src/sections/sections.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Section } from './entities/section.entity';
+import { UsersService } from 'src/users/users.service';
+import { JwtAccessTokenPayloadDto } from 'src/auth/dto/jwt-token-payload.dto';
+import { CreateSectionDto } from './dto/create-section.dto';
+import { ProjectsService } from 'src/projects/projects.service';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class SectionsService {}
+export class SectionsService {
+  constructor(
+    @InjectRepository(Section)
+    private readonly sectionsRepo: Repository<Section>,
+    private readonly usersService: UsersService,
+    private readonly projectsService: ProjectsService,
+  ) {}
+
+  async create(jwtPayload: JwtAccessTokenPayloadDto, data: CreateSectionDto) {
+    const user = await this.usersService.findOneById(jwtPayload.sub);
+    const project = await this.projectsService.findOneByIdAndUserId(
+      data.project_id,
+      user.id,
+    );
+
+    const sectionPayload = {
+      name: data.name,
+      user: user,
+      project: project,
+    };
+
+    const section = await this.sectionsRepo.save(sectionPayload);
+    return section;
+  }
+}
