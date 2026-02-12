be18f1d39585c883550497bc169ee557f8e0034b
João <joaosl.dev@gmail.com>
2026-02-12 15:54:24 -0300

    feat: Add `ProjectsModule` to `SectionsModule` imports

--- src/sections/sections.module.ts
+++ src/sections/sections.module.ts
@@ -5,9 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Section } from './entities/section.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsersModule } from 'src/users/users.module';
+import { ProjectsModule } from 'src/projects/projects.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, TypeOrmModule.forFeature([Section])],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ProjectsModule,
+    TypeOrmModule.forFeature([Section]),
+  ],
   controllers: [SectionsController],
   providers: [SectionsService],
 })
