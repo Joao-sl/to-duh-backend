7589592d7d2c5eed7615ae77403aa64c430cc887
João <joaosl.dev@gmail.com>
2026-04-03 18:06:27 -0300

    chore: remove unused regex and change due_at type to string

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -7,7 +7,6 @@ import {
   IsOptional,
   IsPositive,
   IsString,
-  Matches,
   MaxLength,
   MinLength,
 } from 'class-validator';
@@ -39,8 +38,5 @@ export class CreateTaskDto {
 
   @IsOptional()
   @IsISO8601({ strict: true })
-  @Matches(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/, {
-    message: 'Date must be in format YYYY-MM-DDTHH:mm:ss.SSSZ',
-  })
-  due_at?: Date;
+  due_at?: string;
 }
