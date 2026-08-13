be9aecde21957fe3666e511c526f51a87872afaa
João <joaosl.dev@gmail.com>
2026-08-13 18:09:24 -0300

    feat: add archived to SectionQueryDto

--- src/sections/dto/query-params.dto.ts
+++ src/sections/dto/query-params.dto.ts
@@ -1,9 +1,14 @@
-import { Type } from 'class-transformer';
-import { IsInt, IsOptional } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import { IsBoolean, IsInt, IsOptional } from 'class-validator';
 
 export class SectionQueryDto {
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   project_id?: number;
+
+  @IsOptional()
+  @Transform(({ value }) => value === 'true' || value === true)
+  @IsBoolean()
+  archived?: boolean = false;
 }
